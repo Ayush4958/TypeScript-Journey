ee39cd755f39f728bca6a956703c47dbc141e2ad
Ayush <bhandariayush935@gmail.com>
2025-09-24 21:55:16 +0530

    Data Type in TypeScript

---
+++ lecture-1/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// string data type
+var greetings = "Hello , Ayush";
+// number data type
+// bad practice
+var userId = 45789;
+// good practice
+var UserID = 47858;
+UserID.toFixed();
+//boolean data type
+var loggedIn = true;
+// any data type
+var hero;
+// we will create a more secure function in further lectures
+function gethero() {
+    return "Men are Brave";
+}
+hero = gethero();
