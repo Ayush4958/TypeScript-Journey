803caea7327714861d710917dc9500f53ed0bc64
Ayush <bhandariayush935@gmail.com>
2025-09-24 21:55:34 +0530

    Data Type in TypeScript

---
+++ lecture-1/dataTypes.ts
@@ -0,0 +1,26 @@
+// string data type
+let greetings: string =  "Hello , Ayush";
+
+// number data type
+
+// bad practice
+let userId: number = 45789; 
+
+// good practice
+let UserID = 47858;
+UserID.toFixed();
+
+//boolean data type
+let loggedIn: boolean = true;
+
+// any data type
+let hero;
+
+// we will create a more secure function in further lectures
+function gethero(){
+    return "Men are Brave"
+}
+
+hero = gethero();
+
+export {}
