9c9f286abede956af006583319bdd6afd365ee84
Ayush <bhandariayush935@gmail.com>
2025-09-26 23:53:54 +0530

    Function in TypeScript

--- lecture-2/function.ts
+++ lecture-2/function.ts
@@ -1,1 +1,23 @@
 // Function in ts
+function sumTwo(num : number) {
+    return num + 2;
+}
+sumTwo(8);
+
+function getUpper(val : string){
+    return val.toUpperCase();
+}
+getUpper("HEllo, Ayush");
+
+function signUp(name : string , email :string , pass : string , paid : boolean){
+    return `${name} and his email is ${email} with password which is ${pass} and he is paid user as his status is ${paid}`;
+}
+signUp("Ayush" , "Ayush@example.com" , "123456" , true);
+
+// Arrow function
+let logIn = () =>{
+    return "Hello , Ayush !!!";
+}
+logIn();
+
+export {}; 
