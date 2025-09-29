56edf23dfc3a7e75f3ffda409d6f5423e5c88121
Ayush <bhandariayush935@gmail.com>
2025-09-29 23:17:47 +0530

    Array in TS

---
+++ lecture-4/array.js
@@ -0,0 +1,14 @@
+// Array define as string array
+// 2 methods to declare array with certain data type
+var heroes = [];
+var heroesRank = [];
+heroesRank.push(5);
+heroes.push("Hulk");
+var allUsers = [];
+allUsers.push({ name: "Ayush", active: true });
+// Nested Array Data Type
+// This example will only have number data type array inside an array
+var Models = [
+    [244, 255, 199],
+    [120, 180, 210],
+];
