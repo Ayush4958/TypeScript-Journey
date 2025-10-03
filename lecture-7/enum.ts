fe72ccbeabf58c9d76734565c547c3f8d65d92dc
Ayush <bhandariayush935@gmail.com>
2025-10-03 22:27:21 +0530

    Enum in TS

---
+++ lecture-7/enum.ts
@@ -0,0 +1,8 @@
+const enum SeatChoice {
+    Aisle = 10, // We assign it a specific value here so next value of it will be incremented by  1
+    Middle = 15 , // We can also assign this one specifically , we can also assign it as a string but for that i have to give every one a string value
+    Window , 
+    Fourth = "fourth"
+}
+
+Bseat = SeatChoice.Aisle
