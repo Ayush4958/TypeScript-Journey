a034a31b9ad50af6b3c8835fe4427b91e1b90a7b
Ayush <bhandariayush935@gmail.com>
2025-10-05 23:46:04 +0530

    Interface in TS

---
+++ lecture-8/interface.ts
@@ -0,0 +1,14 @@
+// Interface in TS
+interface user {
+    email : string ,
+    name : string ,
+    userId : number ,
+}
+
+const ayush: user = {
+    email : "ayush@gmail.com" ,
+    name : "Ayush" ,
+    userId : 1 ,
+}
+
+console.log(ayush);
