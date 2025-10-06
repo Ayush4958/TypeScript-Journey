cc8f49bf0cdb24b75c6ad24e190173981cb9775d
Ayush <bhandariayush935@gmail.com>
2025-10-06 23:55:56 +0530

    Interface in TS

--- lecture-8/interface.ts
+++ lecture-8/interface.ts
@@ -5,10 +5,16 @@ interface user {
     userId : number ,
 }
 
-const ayush: user = {
+var ayush: user = {
     email : "ayush@gmail.com" ,
     name : "Ayush" ,
     userId : 1 ,
 }
 
 console.log(ayush);
+
+// ⚠️ When Not to Use Enums
+
+// If the set of values is dynamic (e.g. fetched from an API)
+// If you need more flexibility than enums allow
+// When working in codebases where enums are discouraged in favor of literal union types (e.g. 'admin' | 'user' | 'guest')
