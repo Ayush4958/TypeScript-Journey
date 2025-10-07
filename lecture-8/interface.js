2f085a73b4a75bee7d18637ec7190c3a5c0a0431
Ayush <bhandariayush935@gmail.com>
2025-10-07 23:29:49 +0530

    Interface in TS

--- lecture-8/interface.js
+++ lecture-8/interface.js
@@ -1,6 +1,32 @@
+// Interface in TS
 var ayush = {
+    dbId: 552211,
     email: "ayush@gmail.com",
     name: "Ayush",
     userId: 1,
+    githubToken: "githubAyush1234",
+    StartTrial: function () {
+        return "Trial Started";
+    },
+    getCoupon: function (name, value) {
+        return 10;
+    }
 };
-console.log(ayush);
+var AdminAyush = {
+    role: "TL",
+    dbId: 552211,
+    email: "ayush@gmail.com",
+    name: "Ayush",
+    userId: 1,
+    githubToken: "githubAyush1234",
+    StartTrial: function () {
+        return "Trial Started";
+    },
+    getCoupon: function (name, value) {
+        return 10;
+    }
+};
+// ⚠️ When Not to Use Enums
+// If the set of values is dynamic (e.g. fetched from an API)
+// If you need more flexibility than enums allow
+// When working in codebases where enums are discouraged in favor of literal union types (e.g. 'admin' | 'user' | 'guest')
