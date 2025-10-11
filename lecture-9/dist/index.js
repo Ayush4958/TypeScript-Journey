d33e00854344ab2a66c9f4df72822addbe6caf5f
Ayush <bhandariayush935@gmail.com>
2025-10-11 14:37:57 +0530

    Configured JS File

---
+++ lecture-9/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Its with the help of TypeScript , I made some Changes");
+//# sourceMappingURL=index.js.map
