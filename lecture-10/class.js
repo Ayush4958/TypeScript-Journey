e721223ff47e8f97eb53db37528935512b849114
Ayush <bhandariayush935@gmail.com>
2025-10-12 23:09:10 +0530

    Classes in TypeScript

---
+++ lecture-10/class.js
@@ -0,0 +1,10 @@
+var NewUser = /** @class */ (function () {
+    function NewUser(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+    return NewUser;
+}());
+var ayush = new NewUser("Ayush", "ayushex@.com", 22);
+console.log(ayush);
