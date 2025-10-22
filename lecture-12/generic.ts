3a050ceabbf4bbe64907040bc395858b5899db14
Ayush <bhandariayush935@gmail.com>
2025-10-22 15:10:17 +0530

    Generics in TS

--- lecture-12/generic.ts
+++ lecture-12/generic.ts
@@ -22,3 +22,43 @@ interface Bottle {
 }
 
 identityThree<Bottle>({name:"Miltion" , price:100}) ;
+
+// It's a generic function with array type and we use 2 different ways to define array type 
+function getProducts <T> (products : Array <T>) : T[] {
+    return new Array().concat(...products) ;
+}
+
+// same function but with arrow function syntax
+// ',' used after T for understanding that it's a generic type not any Tag in JSX / TSX
+const getMoreProducts = <T,> (products : T[]) : T[] => {
+     return new Array().concat(...products) ;
+}
+
+// generic function with 2 different type parameters
+function uniqueFunction <T ,U> (param1 : T , param2 : U) : object {
+    return {
+        param1 ,
+        param2 ,
+    };
+}
+uniqueFunction(5 , "5");
+
+// generic function with constraint
+interface Database {
+    connection : string , 
+    id : number , 
+    pass : string ,
+}
+
+// U extend Database means U must have all the properties of Database type 
+function uniqueFunction2 <T , U extends Database> (param1 : T , param2 : U) : object {
+    return {
+        param1 ,
+        param2 ,
+    }
+}
+uniqueFunction2(55 , {
+    connection : "LocalHost:5055" ,
+    id : 100001 ,
+    pass : "Dont give a damn about it" ,
+})
